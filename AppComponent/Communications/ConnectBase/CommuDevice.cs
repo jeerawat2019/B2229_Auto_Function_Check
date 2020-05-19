b021d2944509b9f9e0f8c85e3f42803363e3d7db
jeerawat2019 <iAppMachine2019@gmail.com>
2020-05-19 14:26:25 +0700

    Update Component system

---
+++ AppComponent/Communications/ConnectBase/CommuDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiComp.ConnectType.Commu
+{
+    partial class CommuBase
+    {
+        public enum Device
+        {
+            /// <summary>RS232</summary>
+            RS232,
+        }
+    }
+}
---
+++ AppComponent/Communications/LogSystems/DefaultLogger/LoggerPlugIns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiComp.Logs
+{
+    partial class LoggerBase
+    {
+        public enum PlugIns
+        {
+            /// <summary>Alarm Logger</summary>
+            AlarmLogger,
+
+        }
+    }
+}
